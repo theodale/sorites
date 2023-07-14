7c8ab14783e7b684bdb7fbbe32d89638fb6d75f0
Theo Dale <theomjdale@gmail.com>
2023-07-14 12:24:01 +0100

    fix openzep remapping

--- src/pools/SoritesUniswapV3Pool.sol
+++ src/pools/SoritesUniswapV3Pool.sol
@@ -3,6 +3,4 @@ pragma solidity ^0.8.13;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract SoritesUniswapV3Pool is
-    ERC20("SOR-UNIV3", "Sorites UniswapV3 Share")
-{}
+contract SoritesUniswapV3Pool is ERC20("SOR-UNIV3", "Sorites UniswapV3 Share") {}
